1fe2ca1aa28ba5417e4bd6ad0a884216a02fcbe5
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2020-05-17 20:53:11 +0600

    Add new database

--- Database_Handelar.py
+++ Database_Handelar.py
@@ -1,10 +1,18 @@
+"""
+    this program is created for collecting mail address from a database and
+    write mail addresses in a text file.
+    dependency:
+        1. database.db (sqlite database)
+        2. mail.txt
+"""
+
 import sqlite3
 
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 
 f = open("mail.txt",'a')
-cur.execute("SELECT \"Mail\" FROM Contacts")
+cur.execute("SELECT \"mail\" FROM Contacts")
 row = cur.fetchone()
 
 while row is not None:
